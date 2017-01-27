96687dc1bd42f9a0f09dc4b33e309e08d44283ab
Raphael Amiard <amiard@adacore.com>
2017-01-27 18:14:31 +0100

    Complete chapter 1

--- src/chapter_1.adb
+++ src/chapter_1.adb
@@ -24,7 +24,8 @@ procedure Chapter_1 is
    Z : constant Integer := 0;
    --  You can declare/initialize multiple variables of the same type
    A, B : Integer := 0;
-   --  Named number using <name> : constant := <value>;
+   --  Numeric constant using <name> : constant := <value>;
+   --  Numeric constants have infinite precision.
    Pi : constant := 3.14159_26535_89793_23846_26433_83279_50288_41971_69399;
 
    --------------------
@@ -32,8 +33,14 @@ procedure Chapter_1 is
    --------------------
 
    Int  : constant Integer := -1;
+
+   --  Natural is a subtype of Integer. We'll see what it means exactly later.
+   --  The range of naturals is 0 to Integer'Last
    Nat  : constant Natural := 0;
+
+   --  Positive is a subtype of Integer too.
    Pos  : constant Positive := 1;
+
    F    : Float := 0.0;
    Bool : Boolean := True;
    Char : constant Character := 'c';
@@ -41,6 +48,7 @@ procedure Chapter_1 is
 
    --  Modular types (Unsigned integers) defined in the Interfaces packages
    Byte : Interfaces.Unsigned_8 := 0;
+
 begin
 
    ---------------
@@ -57,7 +65,7 @@ begin
    X := 8#127#; -- Octal
    X := 2#1101_1101_0001_1001#; -- Binary
 
-   --  Infinite precision
+   --  Numeric literals have infinite precision
    X := 10e20_001 / 10e20_000; --  X equals 10
 
    -----------------
@@ -67,21 +75,6 @@ begin
    --  Assignment
    Y := Z;
 
-   --  Conditional expresssion (parentheses are required)
-   B := (if Bool then 0 else A);
-   X := (case B is
-            when 1 => 1,
-            when 2 => 10,
-            when others => 0);
-
-   --  Quantified expressions
-   Bool := (for all Char of Str => Char /= 'e'); -- False
-   Bool := (for some Char of Str => Char = 'e'); -- True
-
-   ---------------
-   -- Operators --
-   ---------------
-
    --  Mathematics
    X := X + 1;
    X := X - 1;
@@ -89,7 +82,7 @@ begin
    X := X / 1;
    X := X**2; -- Exponentiation (power)
 
-   --  Binary
+   --  Binary operations. Those operations only work on modular types.
    Byte := Byte or 2#1111_1111#;
    Byte := Byte and 2#1111_1111#;
    Byte := Byte xor 2#1111_1111#;
@@ -107,6 +100,22 @@ begin
    --  Range membership
    Bool := A in -10 .. 10;
 
+   --  Set membership
+   Bool := A in 1 | 4 | 8;
+
+   --  Declare blocks allow introducing new declarations in a sequence of
+   --  statements.
+   declare
+      --  String (and array) concatenation
+      Str_2 : String := Str & " World!";
+   begin
+      --  Strings and arrays are immutable (cannot change their length), so
+      --  this is illegal:
+      --  Str_2 := Str_2 & "World !";
+
+      --  Prints "Hello World!"
+      Put_Line (Str_2);
+   end;
 
    -------------------------
    --  Control structures --
@@ -152,30 +161,51 @@ begin
 
    --  Case (switches)
    case B is
-      when 0 =>      --  Single value
+      when -9 =>      --  Single value
          A := 0;
-      when 1 | 2 =>  --  Multiple values
+      when -1 | -2 =>  --  Multiple values
          A := 1;
-      when 3 .. 4 => --  Ranges
+      when -3 .. -4 => --  Ranges
          A := 2;
-      when 6 | 8 .. 10 =>
+      when -6 | -8 .. -10 => -- All combinable
          A := 2;
          X := 2;
 
+      when Positive => -- Check for a whole subtype's range
+
+         null; --  Do nothing...
+
       --  Case statement must cover all possible values. If we stop here, the
       --  compiler will give us an error like:
-      --          XX:4 missing case values: -16#8000_0000# .. -1
-      --          XX:4 missing case value: 5
-      --          XX:4 missing case value: 7
-      --          XX:4 missing case values: 11 .. 16#7FFF_FFFF#
+      --  chapter_1.adb:164:04: missing case values: -16#8000_0000# .. -10
+      --  chapter_1.adb:164:04: missing case values: -8 .. -7
+      --  chapter_1.adb:164:04: missing case values: -5 .. -3
+      --  chapter_1.adb:164:04: missing case value: 0
 
       --  Use "when other" to cover all the remaining values
       when others =>
-         null; --  Do nothing...
+         null;
    end case;
 
    --  Goto? Yes, but it's not a reason to use it...
    goto Label;
    <<Label>>
+
+   --  Conditional expresssion (parentheses are required)
+   B := (if Bool then 0 else A);
+
+   --  Case expressions: same as case statements (coverage checking)
+   X := (case B is
+            when 1 => 1,
+            when 2 | 3 => 10,
+            when others => 0);
+
+   --  Quantified expressions
+
+   --  Check that a predicate is true for all values of a container
+   Bool := (for all Char of Str => Char /= 'e'); -- False
+
+   --  Check that a predicate is true for at least one value of a container
+   Bool := (for some Char of Str => Char = 'e'); -- True
 
 end Chapter_1;
